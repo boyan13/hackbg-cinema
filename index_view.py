cec09a62c1f1ea8d9381e48eeaab4acfe0e1b4e2
Boyan Bonev <bonev1717@gmail.com>
2020-05-05 20:22:44 +0300

    Add menu interface
    Add an interface for the main menu with curses module and wip version of the login/sign window

--- index_view.py
+++ index_view.py
@@ -1,15 +1,65 @@
-from users.views import UserViews
+import curses
+import time
 
 
-def welcome():
-    print('Welcome to HackCinema!')
-    command = int(input('Choose a command:\n  1 - log in\n  2 - sign up\n  Input: '))
-    user_views = UserViews()
+menu = ['Login', 'Sign in', 'View Movies', 'Exit']
 
-    if command == 1:
-        return user_views.login()
 
-    if command == 2:
-        return user_views.signup()
+def print_menu(stdscr, selected_row_idx):
+    stdscr.clear()
+    h, w = stdscr.getmaxyx()
 
-    raise ValueError(f'Unknown command {command}.')
+    for idx, row in enumerate(menu):
+        x = w // 2 - len(row) // 2
+        y = h // 2 - len(menu) // 2 + idx
+
+        if idx == selected_row_idx:
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(y, x, row)
+            stdscr.attroff(curses.color_pair(1))
+        else:
+            stdscr.addstr(y, x, row)
+
+    stdscr.refresh()
+
+
+def main_menu(stdscr):
+
+    # Configs
+    curses.curs_set(0)
+
+    # Color Pairs
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    # Menu
+    selected_row = 0
+    while 1:
+        stdscr.clear()
+        print_menu(stdscr, selected_row)
+
+        key = stdscr.getch()
+
+        if key == curses.KEY_UP and selected_row > 0:
+            selected_row -= 1
+        elif key == curses.KEY_DOWN and selected_row < len(menu) - 1:
+            selected_row += 1
+        elif key == curses.KEY_ENTER or key in [10, 13]:
+            stdscr.clear()
+            if selected_row == len(menu) - 1:
+                stdscr.addstr("Goodbye!")
+                stdscr.refresh()
+                time.sleep(1)
+                break
+            else:
+                # TODO parse(menu[selected_row])
+                stdscr.addstr(f"You chose {menu[selected_row]}")
+                stdscr.refresh()
+                time.sleep(3)
+                break
+
+
+def curses_main():
+    curses.wrapper(main_menu)
+
+if __name__ == '__main__':
+    curses.wrapper(main_menu)
--- users/views.py
+++ users/views.py
@@ -1,23 +1,47 @@
-from .controllers import UserContoller
+import curses
+import curses.textpad
+import time
 
 
-class UserViews:
-    def __init__(self):
-        self.controller = UserContoller()
+def get_reg_info(stdscr):
+    stdscr.clear()
+    h, w = stdscr.getmaxyx()
 
-    def login(self):
-        email = input('Email: ')
-        password = input('Password: ')
-        try:
-            self.controller.set_user(email=email, password=password)
-            print("Welcome, hacker.")
-        except RuntimeError as e:
-            print(str(e))
+    dialogue = "Fill in account detail below: WARNING THIS FILE IS WIP!"
+    dialogue_width = w // 2 - len(dialogue) // 2
 
-    def signup(self):
-        email = input('Email: ')
-        password = input('Password: ')
-        try:
-            self.controller.create_user(email=email, password=password)
-        except Exception as e:
-            print(str(e))
+    stdscr.addstr(h // 3, dialogue_width, dialogue)
+
+    email_win = curses.newwin(1, 50, h // 3 + 2, 2)
+    curses.textpad.rectangle(stdscr, h // 3 + 1, 1, h // 3 + 1 + 1 + 1, 1 + 50 + 1)
+
+    password_win = curses.newwin(1, 50, h // 3 + 2, 55)
+    curses.textpad.rectangle(stdscr, h // 3 + 1, 55, h // 3 + 1 + 1 + 1, 55 + 50 + 1)
+
+    stdscr.refresh()
+
+    email_box = curses.textpad.Textbox(email_win)
+    email_box.edit()
+    email = email_box.gather()
+
+    # stdscr.clear()
+    # # height, width, top left (y), top right (x)
+    # win = curses.newwin(5, 30, 2, 1)
+    # # window, top left (y), top left (x), bottom right (y), bottom right (x)
+    # curses.textpad.rectangle(stdscr, 1, 0, 1 + 5 + 1, 1 + 30 + 1)
+    # stdscr.refresh()
+
+    # box = curses.textpad.Textbox(win)
+
+    # box.edit()
+
+    # message = box.gather()
+
+    # stdscr.clear()
+    # stdscr.addstr(message)
+    # stdscr.refresh()
+    # time.sleep(10)
+
+
+if __name__ == '__main__':
+    curses.wrapper(get_reg_info)
