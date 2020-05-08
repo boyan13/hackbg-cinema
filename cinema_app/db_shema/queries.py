76566666c7c8123a7cb685dc6f76d9a9a231af73
SilviyaVasileva <60818785+SilviyaVasileva@users.noreply.github.com>
2020-05-08 12:36:55 +0300

    Changes on functions

--- cinema_app/db_shema/queries.py
+++ cinema_app/db_shema/queries.py
@@ -121,6 +121,14 @@ FROM Reservations
 WHERE projection_id = ?;
 '''
 
+GET_USER_RESERVATIONS = '''
+SELECT r.row, r.col, m.name, p.time, p.date, p.id, r.ID
+FROM Reservations AS r LEFT JOIN 
+Projections AS p ON r.projection_id = p.id LEFT JOIN Movies AS m
+ON p.movie_id = m.id
+WHERE r.user_id =  ?;
+'''
+
 GET_MOVIE_BY_ID = '''
 SELECT *
 FROM Movies
